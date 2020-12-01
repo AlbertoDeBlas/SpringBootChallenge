b31a9c54e4ab15fab8b636afd6fdc2c417f8501e
Alberto de Blas <alberto.de.blas@gmail.com>
2020-12-01 21:32:08 +0100

    refactor: Use Lombok annotations for model classes

--- src/main/java/com/model/Transaction.java
+++ src/main/java/com/model/Transaction.java
@@ -1,13 +1,18 @@
 package com.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
-import java.util.Objects;
 
+@Data
+@AllArgsConstructor    // Empty constructor is needed for Jackson to recreate the object from JSON
+@NoArgsConstructor
 public class Transaction {
 
     @NotNull
@@ -17,51 +22,4 @@ public class Transaction {
     @NotNull
     @PastOrPresent
     private Timestamp timestamp;
-
-    // Empty constructor is needed for Jackson to recreate the object from JSON
-    public Transaction() {
-    }
-
-    public Transaction(BigDecimal amount, Timestamp timestamp) {
-        this.amount = amount;
-        this.timestamp = timestamp;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Timestamp getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Timestamp timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Transaction that = (Transaction) o;
-        return Objects.equals(amount, that.amount) &&
-                Objects.equals(timestamp, that.timestamp);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount, timestamp);
-    }
-
-    @Override
-    public String toString() {
-        return "Transaction{" +
-                "amount=" + amount +
-                ", timestamp=" + timestamp +
-                '}';
-    }
 }
