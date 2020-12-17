ccf770a0d159619b3378c4f726a64c027f25bbf3
Alberto de Blas <alberto.de.blas@gmail.com>
2020-12-17 23:46:28 +0100

    refactor: Migrate to Kotlin StatisticsController.java and fix DistributionManagement in pom

--- src/main/kotlin/com/controller/StatisticsController.kt
+++ src/main/kotlin/com/controller/StatisticsController.kt
@@ -1,33 +1,22 @@
-package com.controller;
+package com.controller
 
-import com.model.Statistics;
-import com.service.StatisticsComputation;
-import com.service.TransactionCache;
+import com.model.Statistics
+import org.springframework.web.bind.annotation.RestController
+import javax.inject.Inject
+import com.service.StatisticsComputation
+import com.service.TransactionCache
+import org.springframework.web.bind.annotation.GetMapping
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.inject.Inject;
-import java.math.BigDecimal;
-import java.util.List;
 
 @RestController
-public class StatisticsController {
-
-    private StatisticsComputation statisticsComputation;
-
-    private TransactionCache transactionCache;
-
-    @Inject
-    public StatisticsController(StatisticsComputation statisticsComputation,
-                                TransactionCache transactionCache) {
-        this.statisticsComputation = statisticsComputation;
-        this.transactionCache = transactionCache;
-    }
-
-    @GetMapping("/statistics")
-    Statistics getStatistics(){
-        List<BigDecimal> amounts = transactionCache.getCacheValues();
-        return statisticsComputation.computeStatistics(amounts);
-    }
+class StatisticsController @Inject constructor(
+    private val statisticsComputation: StatisticsComputation,
+    private val transactionCache: TransactionCache
+) {
+    @get:GetMapping("/statistics")
+    val statistics: Statistics
+        get() {
+            val amounts = transactionCache.cacheValues
+            return statisticsComputation.computeStatistics(amounts)
+        }
 }
