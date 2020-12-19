33239f21ccf1ebaa2073396f0b40e1065c91db52
Alberto de Blas <alberto.de.blas@gmail.com>
2020-12-19 14:51:54 +0100

    refactor: Migrate to Kotlin advice classes

--- src/main/kotlin/com/advice/NotParsableFieldsAdvice.kt
+++ src/main/kotlin/com/advice/NotParsableFieldsAdvice.kt
@@ -1,19 +1,18 @@
-package com.advice;
+package com.advice
 
-import com.fasterxml.jackson.databind.exc.InvalidFormatException;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import com.fasterxml.jackson.databind.exc.InvalidFormatException
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @ControllerAdvice
-public class NotParsableFieldsAdvice {
-
+class NotParsableFieldsAdvice {
     @ResponseBody
-    @ExceptionHandler(InvalidFormatException.class)
+    @ExceptionHandler(InvalidFormatException::class)
     @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
-    String NotParsableFieldsHandler(InvalidFormatException ex){
-        return ex.getMessage();
+    fun NotParsableFieldsHandler(ex: InvalidFormatException): String {
+        return ex.message ?: "Not parsable Fields"
     }
 }
--- src/main/kotlin/com/advice/TransactionValidationAdvice.kt
+++ src/main/kotlin/com/advice/TransactionValidationAdvice.kt
@@ -1,24 +1,27 @@
-package com.advice;
+package com.advice
 
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.http.HttpHeaders
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.http.HttpStatus
+import org.springframework.web.context.request.WebRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.ObjectError
 
 @ControllerAdvice
-public class TransactionValidationAdvice extends ResponseEntityExceptionHandler {
-
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        for(ObjectError error : ex.getBindingResult().getAllErrors()) {
-            if(error.getCode().equals("XSecondsPastOrPresent")){
-                return new ResponseEntity("Validation error", HttpStatus.NO_CONTENT);
+class TransactionValidationAdvice : ResponseEntityExceptionHandler() {
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        for (error in ex.bindingResult.allErrors) {
+            if (error.code == "XSecondsPastOrPresent") {
+                return ResponseEntity("Validation error", HttpStatus.NO_CONTENT)
             }
         }
-        return new ResponseEntity("Validation error", HttpStatus.UNPROCESSABLE_ENTITY);
+        return ResponseEntity("Validation error", HttpStatus.UNPROCESSABLE_ENTITY)
     }
 }
